1c86e7796af1adad1c6756f6190230b1ddbf2840
Kumiameha <148558093+Kumiameha@users.noreply.github.com>
2025-01-08 11:46:11 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+$token = 'ddf62f-f8e9e1-2c37fa-25d595-e4f862'; // Your Token, (Url:http://example.com/Settings).
+$secret = '7Hstqy1diX'; // Your Secret Key, (Url:http://example.com/Settings).
+$RECHPAY_ENVIRONMENT = 'PROD'; // PROD, TEST
+
+
+//1min upi gateway ka php my admin open krna
+
+?>
---
+++ head.php
@@ -0,0 +1,2 @@
+<title>vipwins</title>
+<link rel="shortcut icon" href="/512x512bb.jpg" type="image/x-icon">
